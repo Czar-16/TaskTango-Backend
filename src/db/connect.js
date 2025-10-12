5a762357ac99289c26418ca100192778ff2b1354
Anoop Jha <anoopjha2003@gmail.com>
2025-10-13 02:55:57 +0530

    final commit

---
+++ src/db/connect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("MongoDB Connection Failed ", error.message);
+    process.exit(1); // Stop everything — DB connection failed, don’t continue running the server.
+  }
+};
+
+export default connectDB;
